4645a7383b45b44b3867620f956ebda5ddb3ae70
marcelvth <marcel@configuring.net>
2020-04-21 13:06:08 +0200

    update

--- src/Entity/Blog.php
+++ src/Entity/Blog.php
@@ -3,11 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use App\Mapping\EntityBase;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BlogRepository")
+ * @ORM\HasLifecycleCallbacks
  */
-class Blog
+class Blog extends EntityBase
 {
     /**
      * @ORM\Id()
@@ -36,58 +38,6 @@ class Blog
      */
     private $img_path;
 
-    /**
-     * @var DateTime $created
-     *
-     * @ORM\Column(name="created_at", type="datetime", nullable=false)
-     */
-    protected $createdAt;
-
-    /**
-     * @var DateTime $updated
-     *
-     * @ORM\Column(name="updated_at", type="datetime", nullable=false)
-     */
-    protected $updatedAt;
-
-    /**
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
-     */
-    public function updatedTimestamps(): void
-    {
-        $dateTimeNow = new DateTime('now');
-
-        $this->setUpdatedAt($dateTimeNow);
-
-        if ($this->getCreatedAt() === null) {
-            $this->setCreatedAt($dateTimeNow);
-        }
-    }
-
-    public function getCreatedAt() :?DateTime
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(DateTime $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt() :?DateTime
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(DateTime $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
 
     public function getId(): ?int
     {
